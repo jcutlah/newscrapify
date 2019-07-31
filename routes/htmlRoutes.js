85eadd1df317ba408fcaf500b3d8e83b46f15cdc
James Cutler <jcutler@hubspot.com>
2019-07-31 07:18:10 -0400

    testing handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,6 +2,6 @@ const app = require('express');
 
 module.exports = function(app){
     app.get('/',function(req,res){
-        res.render('index',{title: "Newsify"});
+        res.render('index',{title: "Newsify", list: ["one","two","three"]});
     });
 };
